ed367b179a788ad1ea5fea5b5cba733846600fd6
SejalAgarwal12 <167055307+SejalAgarwal12@users.noreply.github.com>
2024-07-01 12:36:27 +0530

    Update dbConnect.js

--- server/src/configuration/dbConnect.js
+++ server/src/configuration/dbConnect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://sejal1999ag:G2mba1qk2c7wTOp7@mongodb-tutorial.xvzlbar.mongodb.net/Mern_JWT?retryWrites=true&w=majority&appName=mongodb-tutorial")
+mongoose.connect(process.env.MONGO_URL)
 .then(()=>console.log('connected successfully to DB'))
 .catch(err => console.log(err));
 
